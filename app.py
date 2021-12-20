24222f2435e0b2405210d3a530fc60941bc681f3
Joseph Alikah <alikahjoseph@gmail.com>
2021-12-20 18:44:20 +0100

    Added functionality to retire students

--- app.py
+++ app.py
@@ -101,6 +101,8 @@ def students(action=None):
             # Check if the admission number is a valid number
             try:
                 form["admission_no"] = int(form["admission_no"])
+                if form["admission_no"] <= 0:
+                    raise ValueError
             except ValueError:
                 return "TODO: Error"
 
@@ -127,7 +129,42 @@ def students(action=None):
             return redirect("/students/add")
         
         if action == "remove":
-            return "TODO"
+            search_keys = {}
+
+            # Check which values were used in the search
+            for key in request.form:
+                if key == "admission_no":
+                    try:
+                        admission_no = int(request.form[key].strip())
+                        if admission_no <= 0:
+                            raise ValueError
+                    except ValueError:
+                        continue
+                    search_keys[key] = admission_no
+                if request.form.get(key) not in ["", None]:
+                    search_keys[key] = request.form.get(key).strip()
+
+            # Ensure there is at least one search key
+            if len(search_keys) < 1:
+                return "TODO: Error"
+
+            # Generate the query to select relevant students
+            beginning = "SELECT * FROM people JOIN students ON people.id=students.person_id WHERE "
+            conditions = "class IS NOT 'Retired'"
+            for key in search_keys:
+                conditions += " AND "
+                conditions += key + " LIKE ?"
+
+            # Query the database to get the relevant students
+            cursor.execute(beginning + conditions, list(search_keys.values()))
+            students = cursor.fetchall()
+
+            return render_template("student_results.html", students=students, action="/students/delete", title="Remove Student")
+        
+        if action == "delete":
+            cursor.execute("UPDATE students SET class = 'Retired' WHERE admission_no = ?", request.form["admission_no"])
+            connection.commit()
+            return redirect("/")
 
     # If it is a get request determine which form to show
     if action == "choose":
@@ -135,9 +172,9 @@ def students(action=None):
     elif action == "add":
         return render_template("admin/add_student.html", classes=CLASSES)
     elif action == "remove":
-        return "TODO"
+        return render_template("student_search.html", classes=CLASSES, title="Remove Student", action="/students/remove")
     else:
-        return "TODO"
+        return "TODO: Invalid action"
 
 
 @app.route("/about")
